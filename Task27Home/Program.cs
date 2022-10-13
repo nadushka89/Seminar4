528519a0de6afdd4e95fc5417fbe3e7d34b27b90
Надюшка Рыбка <kitti-46@ya.ru>
2022-10-13 23:38:13 +0300

    задача 27 дом

---
+++ Task27Home/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 27: Напишите программу, которая 
+принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+void GetNumber()
+{
+    Console.WriteLine("Введите число:");
+    int number = int.Parse(Console.ReadLine());
+    string numAsString = number.ToString();
+    int sum = 0;
+    int length = numAsString.Length;
+    for (int i = 0; i < length; i++)
+    {
+        sum += int.Parse(numAsString[i].ToString());
+    }
+    Console.WriteLine($"{number}->{sum}");
+}
+GetNumber();
